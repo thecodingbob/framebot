af0a9b91568b236e92b78af5d5b11401c15ce852
thecodingbob <no email for now>
2022-01-22 00:13:57 +0100

    Removed unused function

--- framebot/utils.py
+++ framebot/utils.py
@@ -1,14 +1,9 @@
 import json
-import os
 import shutil
 
 
-def safe_json_dump(fpath, jsoncontent):
+def safe_json_dump(fpath: str, jsoncontent: dict):
     safe_path = fpath + "_safe"
     with open(safe_path, "w") as f:
         json.dump(jsoncontent, f, indent=4)
     shutil.move(safe_path, fpath)
-
-
-def get_filename(full_path):
-    return full_path[full_path.rfind(os.path.sep) + 1:]
