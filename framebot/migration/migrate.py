744b521618bc35695d7adb4d3b001f506b8c6d7f
thecodingbob <vey90@live.it>
2023-01-05 20:49:12 +0100

    Better main files placement

--- framebot/migration/migrate.py
+++ framebot/migration/migrate.py
@@ -1,6 +1,4 @@
-import argparse
 import datetime
-import inspect
 import json
 import os
 import shutil
@@ -97,22 +95,3 @@ def migrate(source_dir: Union[Path, str], target_dir: Union[Path, str] = None):
     print("Done.")
 
 
-def init_argparse() -> argparse.ArgumentParser:
-    arg_parser = argparse.ArgumentParser(
-        prog="Framebot migration tool",
-        usage=f"migrate -s source -t target...",
-        description="Migrates the local file created from old framebot versions for use with the newer ones."
-    )
-    arg_parser.add_argument("-s", "--source", type=Path, metavar="Source directory",
-                        help="Directory where the legacy framebot files are stored.", required=True)
-    arg_parser.add_argument("-t", "--target", type=Path, metavar="Target directory",
-                        help="Directory where the new framebot files are to be stored.", required=False)
-    return arg_parser
-
-
-if __name__ == "__main__":
-    parser = init_argparse()
-    args = parser.parse_args()
-    source = args.source
-    target = args.target if args.target is not None else args.source
-    migrate(source, target)
