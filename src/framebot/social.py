4b6f24df06aeb7c5dad34c8c76e975bf2b7dd64f
thecodingbob <vey90@live.it>
2023-03-11 19:25:05 +0100

    Added documentation, simplified some code

--- src/framebot/social.py
+++ src/framebot/social.py
@@ -1,6 +1,7 @@
 """
 Contains helper methods and classes for social media upload and data gathering
 """
+from dataclasses import dataclass
 from contextlib import contextmanager
 from datetime import timedelta
 from enum import Enum
@@ -15,30 +16,26 @@ from .utils import LoggingObject
 
 
 class RequestMethod(Enum):
+    """
+    Simple enumeration to correctly set request methods
+    """
     GET = "get"
     POST = "post"
 
 
+@dataclass
 class FacebookPostPhotoResponse:
+    """
+    Contains the data the Facebook Graph API response returns after posting a photo
+    """
 
-    def __init__(self, photo_id: str, post_id: str):
-        self.photo_id = photo_id
-        self.post_id = post_id
-
-    @classmethod
-    def from_response_dict(cls, api_response: dict):
-        return cls(api_response["id"], api_response["post_id"])
-
-    def __eq__(self, o: object) -> bool:
-        if type(o) is type(self):
-            return self.__dict__ == o.__dict__
-        return NotImplemented
-
-    def __str__(self):
-        return str(self.__dict__)
-
-    def __repr__(self):
-        return repr(self.__dict__)
+    def __init__(self, api_response: Dict):
+        """
+        Constructor
+        :param api_response: json response from the Graph API
+        """
+        self.photo_id = api_response["id"]
+        self.post_id = api_response["post_id"]
 
 
 class FacebookHelper(LoggingObject):
@@ -55,30 +52,46 @@ class FacebookHelper(LoggingObject):
     STORY_ID_FIELD = "page_story_id"
     REACTIONS_FIELD = "reactions"
 
-    def __init__(self, access_token: str, timeout: timedelta = timedelta(seconds=20)):
+    def __init__(self, access_token: str):
         """
         Constructor
         :param access_token: Access token for the Facebook page
-        :param timeout: timeout for the http requests
         """
         super().__init__()
         self.access_token: str = access_token
         self._init_page_details()
-        # Ignoring the float warning here. The pyfacebook library incorrectly expects an integer, but then passes
-        # the value to the requests module, which in turn expects a float
         self.logger.info(f"Initialized GraphAPI for Facebook. "
                          f"Page id is {self.page_id} and page name is '{self.page_name}'.")
 
-    def _init_page_details(self):
+    def _init_page_details(self) -> None:
+        """
+        Initializes the page details, namely page_name and page_id. Serves also as a fail-fast test for the
+        provided access token.
+        :return:
+        """
         resp = self.get_object(object_id="me", fields=["name,id"])
         self.page_name = resp["name"]
         self.page_id = resp["id"]
 
     def _default_query_params(self) -> Dict:
+        """
+        Provides the default query parameters needed to be included in every request
+        :return: the default query parametrs
+        """
         return {"access_token": self.access_token, "format": "json"}
 
     def _base_request(self, method: RequestMethod, object_id: str, connection: str = None,
                       query_parameters: Dict = None, files: Dict = None, data: Dict = None) -> Dict:
+        """
+        Generic request to the Graph API
+        :param method: the HTTP method for the request
+        :param object_id: Facebook object id, e.g. a post or an album
+        :param connection: e.g. comments or photos
+        :param query_parameters: extra query parameters for the requests
+        :param files: files to upload with the request, e.g. an image
+        :param data: request body. Can contain, for example, a comment
+        :return: The Graph API response in json format
+        """
         if query_parameters is None:
             query_parameters = {}
         query_parameters = {**self._default_query_params(), **query_parameters}
@@ -98,10 +111,24 @@ class FacebookHelper(LoggingObject):
         raise RequestError(response)
 
     def get_object(self, object_id: str, fields: List[str] = None) -> Dict:
+        """
+        Retrieves an object and some of its fields.
+        :param object_id: the wanted Facebook object's id
+        :param fields: wanted fields for the object, e.g. story id or reactions
+        :return: the Graph API json response
+        """
         return self._base_request(RequestMethod.GET, object_id=object_id,
                                   query_parameters={self.FIELDS_QUERY_PARAM: ",".join(fields)})
 
     def post_object(self, object_id: str, connection: str, files: Dict = None, data: Dict = None) -> Dict:
+        """
+        Posts a file or some data
+        :param object_id: the parent object id where to post the object
+        :param connection: e.g. comments or photos
+        :param files: files to upload
+        :param data: data to upload, e.g. a comment
+        :return: the Graph API json response
+        """
         return self._base_request(RequestMethod.POST, object_id=object_id, connection=connection, files=files,
                                   data=data)
 
@@ -113,33 +140,25 @@ class FacebookHelper(LoggingObject):
         """
         return self.get_object(object_id=object_id, fields=[self.STORY_ID_FIELD])[self.STORY_ID_FIELD]
 
-    def post_photo(self, image: Union[Path, str, Image], message: str, album_id: str = "me",
-                   max_retries: int = DEFAULT_MAX_RETRIES, retry_time: timedelta = DEFAULT_RETRY_MINUTES) \
-            -> FacebookPostPhotoResponse:
+    def post_photo(self, image: Union[Path, str, Image], message: str, album_id: str = "me") -> \
+            FacebookPostPhotoResponse:
         """
         Uploads a photo to a specific album, or to the news feed if no album id is specified
-        :param retry_time: time to wait if a failure occurs, before the next retry
-        :param max_retries: max number of retries before giving up
         :param image: The image to be posted. Could be a path to an image file or a PIL Image
         :param message: The message used as image description
         :param album_id: The album where to post the image
         :return the response object containing photo id and post id
         """
-        #TODO: delete
-        print("I'm the new one!")
         if album_id is None:
             album_id = self.page_id
         with open_image_stream(image) as im:
             response = self.post_object(object_id=album_id, connection="photos", files={"source": im},
                                         data={"message": message})
-        return FacebookPostPhotoResponse.from_response_dict(response)
+        return FacebookPostPhotoResponse(response)
 
-    def post_comment(self, object_id: str, image: Union[Path, str, Image] = None, message: str = None,
-                     max_retries: int = DEFAULT_MAX_RETRIES, retry_time: timedelta = DEFAULT_RETRY_MINUTES) -> str:
+    def post_comment(self, object_id: str, image: Union[Path, str, Image] = None, message: str = None) -> str:
         """
         Uploads a comment to a specific post. At least one between image and message must not be None.
-        :param retry_time: time to wait if a failure occurs, before the next retry
-        :param max_retries: max number of retries before giving up
         :param image: The image to be posted. Could be a path to an image file or a PIL Image, or None
         :param message: The comment message, if any
         :param object_id: The post where to append the comment
@@ -157,26 +176,6 @@ class FacebookHelper(LoggingObject):
         else:
             response = self.post_object(object_id=object_id, connection="comments", data={"message": message})
         return response['id']
-
-    # def _post_with_retry(self, object_id: str, connection: str, files: Dict = None, data: Dict = None,
-    #                      max_retries: int = DEFAULT_MAX_RETRIES, retry_time: timedelta = DEFAULT_RETRY_MINUTES) -> Dict:
-    #     retry_count = 0
-    #     while True:
-    #         try:
-    #             return self.post_object(object_id=object_id, connection=connection, files=files, data=data)
-    #         except FacebookError as e:
-    #             if e.code == 190:
-    #                 self.logger.error("Expired access token. Cannot post")
-    #                 raise e
-    #             self.logger.warning("Exception occurred during photo upload.", exc_info=True)
-    #             if retry_count < max_retries:
-    #                 retry_secs = retry_time.total_seconds() if "spam" not in str(e) else retry_time.total_seconds() * 10
-    #                 self.logger.warning(f"Retrying photo upload after {retry_secs} seconds.")
-    #                 time.sleep(retry_secs)
-    #             else:
-    #                 self.logger.error("Unable to post even after several retries. Check what's happening.")
-    #                 raise e
-    #             retry_count += 1
 
     def get_reactions_total_count(self, post_id: str) -> int:
         """
@@ -199,6 +198,11 @@ class FacebookHelper(LoggingObject):
 
 @contextmanager
 def open_image_stream(image: Union[Path, str, Image]) -> Union[bytes, BytesIO]:
+    """
+    Abstracts the image stream opening code between Path objects, strings representing paths and PIL images
+    :param image: a Path object pointing to the image, a string containing the path to the image, or a PIL image
+    :return: a stream for the image data
+    """
     if issubclass(type(image), (str, Path)):
         im_stream = open(image, "rb")
         output = im_stream
@@ -214,6 +218,9 @@ def open_image_stream(image: Union[Path, str, Image]) -> Union[bytes, BytesIO]:
 
 
 class RequestError(Exception):
+    """
+    An error regarding an HTTP request
+    """
 
     def __init__(self, response: Response):
         self.status_code = response.status_code
@@ -222,6 +229,9 @@ class RequestError(Exception):
 
 
 class FacebookError(Exception):
+    """
+    An error from the Facebook Graph API
+    """
 
     def __init__(self, json_data: Dict):
         json_data = json_data["error"]
